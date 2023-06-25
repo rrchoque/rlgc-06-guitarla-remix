e3107850fb1f1b12ccce8f36889a6a5067690c39
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-06-25 00:05:55 -0400

    Se configuró el archivo proncipal root.jsx

---
+++ app/root.jsx
@@ -0,0 +1,61 @@
+import {
+    Meta,
+    Links
+} from '@remix-run/react'
+import styles from './styles/index.css'
+
+export function meta() {
+    return [
+        { charset: "utf-8" },
+        { title: "GuitarLA - Remix" },
+        { name: "viewport", content: "width=device-width,initial-scale=1" },
+    ];
+}
+
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: "https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+        },
+        {
+            rel: 'preconnect',
+            href: "https://fonts.googleapis.com"
+        },
+        {
+            rel: 'preconnect',
+            href: "https://fonts.gstatic.com",
+            crossOrigin: "true"
+        },
+        {
+            rel: 'stylesheet',
+            href: "https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap"
+        },
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
+
+export default function App() {
+  return (
+    <Document>
+        <h1>Hola Mundo</h1>
+    </Document>
+  )
+}
+
+function Document({children}) {
+    return (
+        <html lang="es">
+            <head>
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
