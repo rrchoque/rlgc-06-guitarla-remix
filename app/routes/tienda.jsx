1ac02dfc926d7875d500399f350c4712a96bdc06
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-06-27 00:23:58 -0400

    Se creó el listado de guitarras y se dió formato

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,7 +1,51 @@
+
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarras } from "~/models/guitarras.server"
+import Guitarra from "~/components/guitarra";
+
+import styles from '~/styles/guitarras.css'
+
+export function meta() {
+  return [
+      { title: "GuitarLA - Tienda de Guitarras" },
+      { description: 'GuitarLA - Nuestra colección de guitarras' }
+  ];
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export async function loader() {
+  const guitarras = await getGuitarras();
+
+  console.log(guitarras)
+  return guitarras.data
+}
+
 function Tienda() {
+
+  const guitarras = useLoaderData()
+
   return (
-    <div>
-      Tienda
+    <div className="contenedor">
+      <h2 className="heading">Nuestra Colección</h2>
+
+      { guitarras.length && (
+        <div className="guitarras-grid">
+          { guitarras.map(guitarra => (
+            <Guitarra 
+              guitarra={guitarra?.attributes}
+              key={guitarra?.id}
+            />
+          )) }
+        </div>
+      ) }
     </div>
   )
 }
