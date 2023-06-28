96b56b4eb62d7aedd8ed29122f1d8025e4ea614d
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-06-28 01:10:54 -0400

    Se creó la sección de blog y post del proyecto

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,8 +1,44 @@
+import { useLoaderData } from "@remix-run/react"
+import { getPosts } from "../models/posts.server"
+import Post from "../components/post"
+import styles from '~/styles/blog.css'
+
+export function meta() {
+  return [
+      { title: "GuitarLA - Sobre Nostros" },
+      { description: 'Venta de guitarras, blog de música' }
+  ];
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export async function loader() {
+  const posts = await getPosts()
+  return posts.data
+}
+
 function Blog() {
+  const posts = useLoaderData()
+
   return (
-    <div>
-      Blog
-    </div>
+    <main className="contenedor">
+      <h2 className="heading">Blog</h2>
+      <div className="blog">
+        {posts.map(post => (
+          <Post 
+            key={post.id} 
+            post={post.attributes} 
+          />
+        ))}
+      </div>
+    </main>
   )
 }
 
